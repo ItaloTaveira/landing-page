16d0bd59f040cf7aa1f4e518d52b97e5da0db3a2
Italo Taveira <italotaveira@outlook.com>
2025-09-07 15:11:11 -0300

    updates

--- landing/vite.config.js
+++ landing/vite.config.js
@@ -7,7 +7,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/landing-page/' // coloque aqui o nome EXATO do seu repositório
+  base: '/landing-page/' // nome exato do seu repositório
+ // coloque aqui o nome EXATO do seu repositório
 })
 
 
