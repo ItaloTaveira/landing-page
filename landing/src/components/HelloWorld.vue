f7ef44f2a43c14cc63a7b0298b8eb0fbf0f58eea
Italo Taveira <italotaveira@outlook.com>
2025-09-07 14:11:31 -0300

    primeiro commit

---
+++ landing/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <v-row>
+    <v-col cols="6">
+      <p>
+        
+      </p>
+
+    </v-col>
+  </v-row>
+ 
+</template>
---
+++ landing/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/meu-projeto/' // coloque aqui o nome EXATO do seu repositório
+})
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
